8d4129dc42589cbbbad607e65dfa699ec0e061c6
eyadras98 <eyadras1998@gmail.com>
2025-10-20 12:51:19 +0300

    feat(infrastructure): add AppDbContext and reference Domain project

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        
+    }
+}
